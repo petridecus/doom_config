49972d5f792517e98e9e41c86f466eef51a92522
Andreas Petrides <apetrides32@gmail.com>
2021-11-14 15:52:42 -0500

    finished setting up irony linting and .editorconfig stuff

---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(editorconfig rtags flycheck-irony company-irony-c-headers company-irony company-box company-dict company irony)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
